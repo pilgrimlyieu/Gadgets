861817c0b2354f12ad2e02f5a4f3f8ac2add8d1d
PilgrimLyieu <PilgrimLyieu@outlook.com>
2024-02-10 12:07:07 +0800

    Release `stats.py` v1.0.0
    Added:
    1. Version, Author and Lincense etc. information
    2. Timeout option `-t` `--timeout` to set the timeout for the process
    
    Changed:
    1. Format code
    2. Don't ignore space if regex is used or library including spaces used
    3. Skip processing non-text files
    4. `-f` `--format` no longer work for files included directly
    5. Enhance argument parsing
    6. Improve help message
    
    Fixed:
    1. Error throwing when processing binary files
    2. Wrong `-i` `--case-sensitive` option(should be `--case-insensitive`)

--- stats.py
+++ stats.py
@@ -4,41 +4,67 @@ import argparse
 import os
 import re
 import sys
+import signal
+import magic
 from collections import Counter
 from fnmatch import fnmatch
 
-# 定义不同字符集
+__version__ = "v1.0.0"
+__year__    = 2024
+__license__ = "MIT"
+__author__  = "Lyieu"
+
 libraries = {
-    "c": r"[^\W]",  # 所有可打印字符
-    "cp": r"[^\W]|[\s]",  # 所有可打印字符和空格字符
-    "cn": r"[\u4e00-\u9fff]",  # 所有常用汉字
-    "en": r"[a-zA-Z]",  # 所有英文字母
-    "alnum": r"[a-zA-Z\d]",  # 字母和数字字符
-    "num": r"[\d]",  # 所有数字
-    "sp": r"[\s]",  # 空白字符
-    "punc": r"[^\w\s]",  # 标点字符
+    "c":     r"[^\W]",           # 所有可打印字符
+    "cp":    r"[^\W]|[\s]",      # 所有可打印字符和空格字符
+    "cn":    r"[\u4e00-\u9fff]", # 所有常用汉字
+    "en":    r"[a-zA-Z]",        # 所有英文字母
+    "alnum": r"[a-zA-Z\d]",      # 字母和数字字符
+    "num":   r"[\d]",            # 所有数字
+    "sp":    r"[\s]",            # 空白字符
+    "punc":  r"[^\w\s]",         # 标点字符
 }
 
-def process_file(file, regex, library, ignore_space, ignore_case, verbose):
+def debug_info(message, verbose=False):
     if verbose:
-        print(f"Processing file: {file}")
-    with open(file, 'r', encoding='utf-8') as f:
-        content = f.read()
-        if ignore_space:
-            content = re.sub(r'\s', '', content)
-        if ignore_case:
-            content = content.lower()
-        if regex:
-            matches = re.findall(regex, content)
-        elif library:
-            matches = re.findall(libraries[library], content)
-        else:
-            matches = list(content)
-        return matches
+        print(message)
 
-def main():
-    parser = argparse.ArgumentParser(description='Count the occurrences of characters in files.',
-                                     epilog='''libraries:
+def signal_handler(signum, frame):
+    raise TimeoutError("Execution time exceeded the timeout.")
+
+def file_statistics(file, regex, library, ignore_space, ignore_case, verbose):
+    debug_info(f"Processing file: {file}", verbose)
+    file_type = magic.from_file(file)
+    if "text" not in file_type:
+        debug_info(f"Skipping file due to non-text type: {file}", verbose)
+        return None
+    try:
+        with open(file, 'r', encoding='utf-8', errors='ignore') as f:
+            content = f.read()
+            if ignore_space and not regex and  library not in ["cp", "sp"]:
+                content = re.sub(r'\s', '', content)
+            if ignore_case:
+                content = content.lower()
+            if regex:
+                matches = re.findall(regex, content)
+            elif library:
+                matches = re.findall(libraries[library], content)
+            else:
+                matches = list(content)
+            return matches
+    except UnicodeDecodeError:
+        print(f"Skipping file due to encoding error: {file}")
+        return None
+    except TimeoutError:
+        print(f"Execution time exceeded the timeout.")
+        sys.exit(1)
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+                        description='Count the occurrences of characters in files.',
+                        epilog=
+f'''
+libraries:
   c           All printable characters.
   cp          All printable and space characters.
   cn          All common Chinese characters.
@@ -46,46 +72,129 @@ def main():
   alnum       Alphabetic and numeric characters.
   num         Numeric characters.
   sp          Space characters.
-  punc        Punctuation characters.''',
-  formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('-n', '--number', metavar="number", type=int, default=0, help='The number of most common characters to display.')
-    parser.add_argument('-e', '--expression', metavar="regex", type=str, default="", help='The regular expression to match.')
-    parser.add_argument('-l', '--library', metavar="library", type=str, choices=libraries.keys(), help='The character set library to use.')
-    parser.add_argument('-f', '--format', metavar="format", type=str, default="", help='The file formats to process.')
-    parser.add_argument('-o', '--output', metavar="output", type=str, default="", help='The output file.')
-    parser.add_argument('-r', '--reverse', action='store_true', default=False, help='Reverse the order of the output.')
-    parser.add_argument('-R', '--recursive', action='store_true', default=False, help='Recursively process directories.')
-    parser.add_argument('-S', '--show-space', action='store_true', default=False, help='Show whitespace characters.')
-    parser.add_argument('-i', '--case-sensitive', action='store_true', default=False, help='Ignore case when matching.')
-    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Display verbose output.')
-    parser.add_argument('paths', nargs='+', help='The files or directories to process.')
+  punc        Punctuation characters.
+
+Copyright (c) {__year__} {__author__} under {__license__} license.
+''',
+                        formatter_class=argparse.RawTextHelpFormatter)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        '-e', '--expression',
+        metavar="regex",
+        type=str,
+        default="",
+        help='The regular expression to match.'
+    )
+    group.add_argument(
+        '-l', '--library',
+        metavar="library",
+        type=str,
+        choices=libraries.keys(),
+        help='The character set library to use.'
+    )
+    parser.add_argument(
+        '-f', '--format',
+        metavar="format",
+        type=str,
+        default="",
+        help='The file formats to process. Use \',\' to separate multiple formats(e.g. "-f txt,md").'
+    )
+    parser.add_argument(
+        '-n', '--number',
+        metavar="number",
+        type=int,
+        default=0,
+        help='The number of most common characters to display.'
+    )
+    parser.add_argument(
+        '-r', '--reverse',
+        action='store_true',
+        default=False,
+        help='Reverse the order of the output.'
+    )
+    parser.add_argument(
+        '-R', '--recursive',
+        action='store_true',
+        default=False,
+        help='Recursively process directories.'
+    )
+    parser.add_argument(
+        '-S', '--show-space',
+        action='store_true',
+        default=False,
+        help='Show whitespace characters.'
+    )
+    parser.add_argument(
+        '-i', '--case-insensitive',
+        action='store_true',
+        default=False,
+        help='Ignore case when matching.'
+    )
+    parser.add_argument(
+        '-t', '--timeout',
+        metavar="seconds",
+        type=int,
+        default=60,
+        help='The maximum time to process a file.'
+    )
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        default=False,
+        help='Display verbose output.'
+    )
+    parser.add_argument(
+        '-V', '--version',
+        action='version',
+        version=f'%(prog)s {__version__}',
+        help='Display version information and exit.'
+    )
+    parser.add_argument(
+        '-o', '--output',
+        metavar="output",
+        type=str,
+        default="",
+        help='The output file.'
+    )
+    parser.add_argument(
+        'paths',
+        nargs='+',
+        type=str,
+        default="",
+        help='The files or directories to process.'
+    )
     args = parser.parse_args()
+    return args
 
-    if args.expression and args.library:
-        print("Error: --expression and --library options cannot be used together.")
-        sys.exit(1)
+def process_file(file, expression, library, ignore_space, ignore_case, verbose, results, processed_files):
+    statistics = file_statistics(file, expression, library, ignore_space, ignore_case, verbose)
+    if statistics is not None:
+        results.extend(statistics)
+        processed_files.append(file)
 
-    if args.library and args.library not in libraries:
-        print(f"Error: Unknown library --{args.library}")
-        sys.exit(1)
+def process_directory(directory, expression, library, ignore_space, ignore_case, recursive, file_formats, verbose, results, processed_files):
+    for root, dirs, files in os.walk(directory):
+        for name in files:
+            if not file_formats or any(fnmatch(name, f'*.{fmt}') for fmt in file_formats):
+                process_file(os.path.join(root, name), expression, library, ignore_space, ignore_case, verbose, results, processed_files)
+        if not recursive:
+            break
+
+def main():
+    args = parse_args()
+    
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(args.timeout)
 
     file_formats = args.format.split(',') if args.format else []
 
     results = []
-    processed_files = []  # 新增一个列表用于存储处理过的文件
+    processed_files = []
     for path in args.paths:
         if os.path.isfile(path):
-            if not file_formats or any(fnmatch(path, f'*.{fmt}') for fmt in file_formats):
-                results.extend(process_file(path, args.expression, args.library, not args.show_space, not args.case_sensitive, args.verbose))
-                processed_files.append(path)  # 处理完一个文件后，将其添加到列表中
+            process_file(path, args.expression, args.library, not args.show_space, args.case_insensitive, args.verbose, results, processed_files)
         elif os.path.isdir(path):
-            for root, dirs, files in os.walk(path):
-                for name in files:
-                    if not file_formats or any(fnmatch(name, f'*.{fmt}') for fmt in file_formats):
-                        results.extend(process_file(os.path.join(root, name), args.expression, args.library, not args.show_space, not args.case_sensitive, args.verbose))
-                        processed_files.append(os.path.join(root, name))  # 处理完一个文件后，将其添加到列表中
-                if not args.recursive:
-                    break
+            process_directory(path, args.expression, args.library, not args.show_space, args.case_insensitive, args.recursive, file_formats, args.verbose, results, processed_files)
 
     counter = Counter(results)
     most_common = counter.most_common(args.number if args.number > 0 else None)
@@ -99,7 +208,6 @@ def main():
     if args.output:
         f.close()
 
-    # 在所有文件处理完后，输出处理过的文件列表
     print("Processed files:")
     for file in processed_files:
         print(file)
